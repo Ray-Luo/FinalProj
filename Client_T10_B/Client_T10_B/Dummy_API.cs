d73e2fae984e6c776b78f18645138a01f68ab75c
Ray-Luo <leiluo.ray@gmail.com>
2017-04-21 23:18:18 -0500

    more methods for dummy api

--- Client_T10_B/Client_T10_B/Dummy_API.cs
+++ Client_T10_B/Client_T10_B/Dummy_API.cs
@@ -17,5 +17,53 @@ namespace Client_T10_B
             return null;
 
         }
+
+        public string logout(string json)
+        {
+            return null;
+
+        }
+
+        public string createUser(string json)
+        {
+            return null;
+
+        }
+
+        public string createChat(string json)
+        {
+            return null;
+
+        }
+
+        public string addChatMember(string json)
+        {
+            return null;
+
+        }
+
+        public string leaveChat(string json)
+        {
+            return null;
+
+        }
+
+        public string chatMessage(string json)
+        {
+            return null;
+
+        }
+
+        public string contactAdded(string json)
+        {
+            return null;
+
+        }
+
+        public string contactRemoved(string json)
+        {
+            return null;
+
+        }
     }
 }
