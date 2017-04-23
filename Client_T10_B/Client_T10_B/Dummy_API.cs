87056f165d7349c0b40cac5004f870a4d89f07d3
virashree <viru@ksu.edu>
2017-04-23 14:44:42 -0500

    added enum

--- Client_T10_B/Client_T10_B/Dummy_API.cs
+++ Client_T10_B/Client_T10_B/Dummy_API.cs
@@ -5,15 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
+
 namespace Client_T10_B
 {
     class Dummy_API
     {
         public Dummy_API()
-        { }
+        {
+
+        }
 
         public string login(string json)
         {
+
             return null;
 
         }
--- Client_T10_B/Client_T10_B/Program.cs
+++ Client_T10_B/Client_T10_B/Program.cs
@@ -12,7 +12,9 @@ namespace Client_T10_B
         public delegate void Observer();
         // defines the type of method that handles an input event (button press):
         public delegate void InputHandler(object sender, EventArgs e);
-
+        
+        // message types for the server client sync 
+        public enum messageType { login, logOut, statusChange, roomStatusChange, createChat, addChatMember, leaveChat, chatMessage, contactAdded, contactRemoved };
 
         /// <summary>
         /// The main entry point for the application.
