9bd76319078664e699eb32c830bd08e3d8ecd88a
Ray-Luo <leiluo.ray@gmail.com>
2017-04-24 13:59:46 -0500

    Chatbox ui

---
+++ Client_T10_B/Client_T10_B/Chatbox_v.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Client_T10_B
+{
+    partial class Chatbox_v
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.uxLeave = new System.Windows.Forms.Button();
+            this.uxAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.uxSend = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.uxAdd);
+            this.splitContainer1.Panel1.Controls.Add(this.uxLeave);
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(735, 548);
+            this.splitContainer1.SplitterDistance = 208;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.uxSend);
+            this.splitContainer2.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(523, 548);
+            this.splitContainer2.SplitterDistance = 471;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(202, 500);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(517, 472);
+            this.listBox1.TabIndex = 0;
+            // 
+            // uxLeave
+            // 
+            this.uxLeave.Location = new System.Drawing.Point(12, 513);
+            this.uxLeave.Name = "uxLeave";
+            this.uxLeave.Size = new System.Drawing.Size(75, 23);
+            this.uxLeave.TabIndex = 1;
+            this.uxLeave.Text = "Leave";
+            this.uxLeave.UseVisualStyleBackColor = true;
+            // 
+            // uxAdd
+            // 
+            this.uxAdd.Location = new System.Drawing.Point(115, 513);
+            this.uxAdd.Name = "uxAdd";
+            this.uxAdd.Size = new System.Drawing.Size(75, 23);
+            this.uxAdd.TabIndex = 2;
+            this.uxAdd.Text = "Add";
+            this.uxAdd.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(439, 64);
+            this.textBox1.TabIndex = 0;
+            // 
+            // uxSend
+            // 
+            this.uxSend.Location = new System.Drawing.Point(448, 3);
+            this.uxSend.Name = "uxSend";
+            this.uxSend.Size = new System.Drawing.Size(75, 64);
+            this.uxSend.TabIndex = 1;
+            this.uxSend.Text = "Send";
+            this.uxSend.UseVisualStyleBackColor = true;
+            // 
+            // Chatbox_v
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 548);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Chatbox_v";
+            this.Text = "Chatbox_v";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button uxAdd;
+        private System.Windows.Forms.Button uxLeave;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button uxSend;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Client_T10_B/Client_T10_B/Chatbox_v.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_T10_B
+{
+    public partial class Chatbox_v : Form
+    {
+        public Chatbox_v()
+        {
+            InitializeComponent();
+        }
+    }
+}
